36ef413f41d8f0a03e1a200c4d009b9053346deb
brandon lindberg <lindberg.scott.brandon@gmail.com>
2021-01-15 14:25:44 +0900

    added destory and image card for plant

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do 
-  get 'plants/create'
-  get 'plants/destroy'
   resources :gardens, only: [:index, :show] do
     resources :plants, only: [:create]
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,18 +12,18 @@ end
 
 puts "Creating new gardens..."
 Garden.create!(
-  name: "Shnjuku Garden, Tokyo",
-  banner_url: "https://img.theculturetrip.com/768x/smart/wp-content/uploads/2017/01/saiho-ji1.jpg"
+  name: "Shinjuku Garden, Tokyo",
+  banner_url: "https://images.unsplash.com/photo-1476055090065-a605fefd840e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
 )
 
 Garden.create!(
   name: "Kourakuen, Okayama",
-  banner_url: "https://img.theculturetrip.com/768x/smart/wp-content/uploads/2017/01/ginkakuji.jpg"
+  banner_url: "https://www.okayama-japan.jp/cms/wp-content/uploads/2015/11/e89bcc7802d5a5f63eb6d0684b1ee014-895x600.jpg"
 )
 
 Garden.create!(
   name: "Risturin, Takamatsu",
-  banner_url: "https://img.theculturetrip.com/768x/smart/wp-content/uploads/2017/01/tenryuji.jpg"
+  banner_url: "https://www.setouchiexplorer.com/wp-content/uploads/2012/10/Ritsurin-Garden-in-November-600x444.jpg"
 )
 
 puts "Seeding gardens...done"
